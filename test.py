9a5a5021de50b4bbfc4fe8f7f26bddff7ce8c50b
arno06 <arno06@gmail.com>
2018-07-23 20:51:28 +0200

    Completion du canvas

--- test.py
+++ test.py
@@ -6,6 +6,11 @@ str="20:23"
 
 canvas = HeightPixelFont.from_string(str)
 
+diff = 32 - len(canvas)
+
+for i in range(diff):
+    canvas.append([0 for k in range(8)])
+
 matrix = MAX7219()
 matrix.set_canvas(canvas)
 
