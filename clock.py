21afa57cd007f45f4bc29c49a35bbac3624d8550
arno06 <arno06@gmail.com>
2018-07-27 20:10:19 +0200

    Horloge via un daemon

---
+++ clock.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+from max7219 import MAX7219
+from max7219 import HeightPixelFont
+import sys
+import logging
+import os
+import time
+
+from daemons.prefab import run
+
+
+class ClockDaemon(run.RunDaemon):
+
+    def __init__(self, pidfile):
+        super(ClockDaemon, self).__init__(pidfile=pidfile)
+        self.matrix = MAX7219()
+
+    def run(self):
+        while True:
+            self.set_time(":")
+            time.sleep(0.5)
+            self.set_time(" ")
+            time.sleep(0.5)
+
+    def set_time(self, separator):
+        t = time.localtime()
+        hour = str(t.tm_hour)
+        if t.tm_hour < 10:
+            hour = "0"+hour
+        min = str(t.tm_min)
+        if t.tm_min<10:
+            min = "0"+min
+        time_str = hour+separator+min
+        canvas = HeightPixelFont.from_string(time_str)
+        diff = 32 - len(canvas)
+        for i in range(diff):
+            canvas.append([0 for k in range(8)])
+        self.matrix.set_canvas(canvas)
+
+    def close(self):
+        self.matrix.close()
+        self.stop()
+
+
+if __name__ == '__main__':
+    action = sys.argv[1]
+    logfile = os.path.join(os.getcwd(), "clock.log")
+    pidfile = os.path.join(os.getcwd(), "clock.pid")
+
+    logging.basicConfig(filename=logfile, level=logging.DEBUG)
+    d = ClockDaemon(pidfile=pidfile)
+
+    if action == "start":
+        print("starting "+pidfile)
+        d.start()
+    elif action == "stop":
+        print("stop "+pidfile)
+        d.close()
+    elif action == "restart":
+        d.restart()
